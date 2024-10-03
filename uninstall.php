572afa2dd69cf7fa434ce016a2b9cb1da6b05fb3
Gareth J M Saunders <garethjmsaunders@gmail.com>
2024-10-03 20:10:43 +0100

    FIX (Uninstall) Remove multisite
    Elegant Themes raised the following issue
    On line: 17 Overriding WordPress globals is prohibited. Found assignment to $blog_id WordPress.WP.GlobalVariablesOverride.Prohibited
    
    Remove multisite code. This was never intended to be a multisite plugin.

--- uninstall.php
+++ uninstall.php
@@ -9,14 +9,3 @@ $option_name = 'divi_projects_cpt_rename_settings';
 
 // Delete the option from the database
 delete_option( $option_name );
-
-// For multisite
-if ( is_multisite() ) {
-    global $wpdb;
-    $blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
-    foreach ( $blog_ids as $blog_id ) {
-        switch_to_blog( $blog_id );
-        delete_option( $option_name );
-        restore_current_blog();
-    }
-}
